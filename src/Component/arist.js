94ccc05e8ae13b4f88647ad91d7528500902c0a7
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2020-04-05 07:43:58 +0100

    0.1

---
+++ src/Component/arist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Artist extends Component{
+    render(){
+        return(
+            <div>
+               artist
+            </div>
+        )
+    }
+}
+
+export default Artist;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+import { BrowserRouter, Route} from 'react-router-dom';
+
+import Home from './Component/home';
+import Artist from './Component/arist';
+
+
+const App = () =>{
+  return(
+    <BrowserRouter>
+      <div>
+         <Route exact path="/" component = {Home} />
+         <Route path = "artist/:id" component = {Artist} />
+      </div>
+    </BrowserRouter>
+  )
+}
+ReactDOM.render(<App />,document.getElementById('root')
+);
+
+
