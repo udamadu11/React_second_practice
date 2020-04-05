16da83832bfe14a055e4ecbc6fca78cfca2d61cd
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2020-04-05 16:25:44 +0100

    0.3

---
+++ src/Component/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List = (props) => {
+   
+    console.log(props)
+
+    return(
+       <div>List</div>
+   )
+}
+export default List;
--- src/Component/home.js
+++ src/Component/home.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import Banner from './banner';
+import List from './List';
+const url_Art = 'http://localhost:3301/artists';
+
 class Home extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            artists: ''
+        }
+    }
+
+    componentDidMount(){
+        fetch(url_Art, {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json =>{
+            this.setState({
+                artists: json
+            })
+        })
+    }
+
     render(){
         return(
             <div>
                 <Banner></Banner>
+                <List allArtist={this.state.artists}></List>
             </div>
         )
     }
 } 
-
 
 export default Home;
